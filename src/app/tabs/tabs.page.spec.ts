ebdb920505cbc3ff6ac6ebd6eb7406424b9efcb0
Doron Tal <5935957+dorontal@users.noreply.github.com>
2021-08-31 16:55:32 -0400

    packages upgraded & tests pass

--- src/app/tabs/tabs.page.spec.ts
+++ src/app/tabs/tabs.page.spec.ts
@@ -1,26 +1,31 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 import { TabsPage } from './tabs.page';
 
 describe('TabsPage', () => {
-  let component: TabsPage;
-  let fixture: ComponentFixture<TabsPage>;
+    let component: TabsPage;
+    let fixture: ComponentFixture<TabsPage>;
+    let router: Router;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [TabsPage],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
-  }));
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [TabsPage],
+            imports: [RouterTestingModule],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TabsPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TabsPage);
+        component = fixture.componentInstance;
+        router = TestBed.inject(Router);
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
